bca6b99f7c70193a4687d9a53abb7da2808675c0
Jithendranath8 <145124590+Jithendranath8@users.noreply.github.com>
2025-01-03 21:39:06 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://peer-chat-9ocp.vercel.app",
+    origin: '*',
     credentials: true,
   })
 );
